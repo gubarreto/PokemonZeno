c5fc4bf30ed79efabadd814350555347f087fdd2
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-09-14 11:45:25 -0300

    Initial Commit

---
+++ PokemonZeno/PokemonZenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonZenoApp.swift
+//  PokemonZeno
+//
+//  Created by Gustavo on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonZenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
